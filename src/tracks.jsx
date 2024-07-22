ac1ac47b097b0b1b6e2a49de3644ae75a7515671
marknaman05 <marknaman05@gmail.com>
2024-07-23 00:11:53 +0530

    public fixes

--- src/tracks.jsx
+++ src/tracks.jsx
@@ -2,77 +2,77 @@ import React from 'react'
 
 const tracks = [
     {
-      url: "public/sfx/oooo.mp3",
+      url: "/sfx/oooo.mp3",
       title: "OoooOoo",
       tags: ["Garma"],
     },
     {
-      url: "public/sfx/bhai-sorry.mp3",
+      url: "/sfx/bhai-sorry.mp3",
       title: "Bhai Sorry",
       tags: ["Aand"],
     },
     {
-      url: "public/sfx/sach-bata",
+      url: "/sfx/sach-bata",
       title: "Sach Bata",
       tags: ["Garma"],
     },
     {
-      url: "public/sfx/bhai-vidhan-kya-chal-raha-h.mp3",
+      url: "/sfx/bhai-vidhan-kya-chal-raha-h.mp3",
       title: "Bhai Vidhan Kya Chal Raha Hai",
       tags: ["Garma"],
     },
     {
-      url: "public/sfx/vidhan-ka-hinge.mp3",
+      url: "/sfx/vidhan-ka-hinge.mp3",
       title: "Bhai Vidhan Ka Hinge",
       tags: ["Garma"],
     },
     {
-      url: "public/sfx/O-yeah.mp3",
+      url: "/sfx/O-yeah.mp3",
       title: "Ooo Yeahh",
       tags: ["Gunnu"],
     },
     {
-      url: "public/sfx/awww.mp3",
+      url: "/sfx/awww.mp3",
       title: "Awww by Garma",
       tags: ["Garma"],
     },
     {
-      url: "public/sfx/go-for-her.mp3",
+      url: "/sfx/go-for-her.mp3",
       title: "Go For Her bro",
       tags: ["Gunnu"],
     },
     {
-      url: "public/sfx/Yes-bro-actually.mp3",
+      url: "/sfx/Yes-bro-actually.mp3",
       title: "Actually... by garma",
       tags: ["Garma"],
     },
     {
-      url: "public/sfx/horny-garma.mp3",
+      url: "/sfx/horny-garma.mp3",
       title: "Horny Garma",
       tags: ["Garma"],
     },
     {
-      url: "public/sfx/aaj-kya.mp3",
+      url: "/sfx/aaj-kya.mp3",
       title: "Aaj Kya Pehna H",
       tags: ["Tanshu"],
     },
     {
-      url: "public/sfx/Oooo-song.mp3",
+      url: "/sfx/Oooo-song.mp3",
       title: "Oooo song by Garma",
       tags: ["Garma"],
     },
     {
-      url: "public/sfx/horny-garma-2.mp3",
+      url: "/sfx/horny-garma-2.mp3",
       title: "Horny Garma 2.0",
       tags: ["Garma"],
     },
     {
-      url: "public/sfx/Video-delete.mp3",
+      url: "/sfx/Video-delete.mp3",
       title: "Video Delete by Katti",
       tags: ["Katti"],
     },
     {
-      url: "public/sfx/hariOm.mp3",
+      url: "/sfx/hariOm.mp3",
       title: "Hari Om",
       tags: ["Hurshil"],
     },
