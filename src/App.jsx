98d1d0dd749e90d1ae7271bfab95f01ffcdb6214
marknaman05 <marknaman05@gmail.com>
2024-07-23 00:06:37 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Player from "@madzadev/audio-player";
 import "@madzadev/audio-player/dist/index.css";
